3ea68fe51f152d333eb5d3ad3e99c0b063fc4e27
Ryca77 <ryan.carter77@gmail.com>
2016-11-25 17:42:57 +0000

    added safe points and broadened area for random infection point

--- js/components/survival-map.js
+++ js/components/survival-map.js
@@ -6,7 +6,7 @@ var SurvivalMap = React.createClass({
         return {
             userCoords: {},
             infectionCoords: {},
-            safeCoords: []
+            safePlaceCoords: []
         }
     },
 
@@ -22,21 +22,32 @@ var SurvivalMap = React.createClass({
             var userCoords = {lat: latitude, lng: longitude};
             this.setState({userCoords: userCoords});
         });
-        //random latitudes  51.2 to 54
-    	var randomLat = (Math.random()*(57.468 - 54.965) + 54.965.toFixed() * 1);
-
-    	
-    	console.log(randomLat);
-        //random longituded -0.1 to 2.2
-        var randomLng = (Math.random()*(-3.054 - -4.681) + -4.681.toFixed() * 1);
-        console.log(randomLng);
-        var infectionCoords = {lat: randomLat, lng: randomLng};
+        //random lats and lngs - scotland, wales, west, east
+    	var randomLatLng =  [
+    		{lat: (Math.random()*(57.468 - 54.965) + 54.965.toFixed() * 1), lng: (Math.random()*(-3.054 - -4.681) + -4.681.toFixed() * 1)},
+    		{lat: (Math.random()*(53.186 - 51.713) + 51.713.toFixed() * 1), lng: (Math.random()*(-2.614 - -4.021) + -4.021.toFixed() * 1)},
+    		{lat: (Math.random()*(54.495 - 50.875) + 50.875.toFixed() * 1), lng: (Math.random()*(-0.747 - -2.416) + -2.416.toFixed() * 1)},
+			{lat: (Math.random()*(52.736 - 50.972) + 50.972.toFixed() * 1), lng: (Math.random()*(-0.593 - -0.746) + -0.746.toFixed() * 1)}
+		];
+        var infectionCoords = randomLatLng[Math.floor(Math.random() * randomLatLng.length)];
+        console.log(infectionCoords);
         this.setState({infectionCoords: infectionCoords});
+        //lats and lngs for safe places
+        var safePlaceCoords = [
+        	{lat: 51.891796, lng: 0.901473},
+        	{lat: 55.9022, lng: -3.2395},
+        	{lat: 57.583889, lng: -4.070278},
+        	{lat: 54.377634, lng: -1.723628},
+        	{lat: 52.823333, lng: -2.144722},
+        	{lat: 51.2599, lng: -0.7598}
+        ];
+        this.setState({safePlaceCoords: safePlaceCoords});
 	},
 
 	generateMap: function() {
 		var userCoords = this.state.userCoords;
 		var infectionCoords = this.state.infectionCoords;
+		var safePlaceCoords = this.state.safePlaceCoords;
 		this.mapImage = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 54.559322, lng: -2.5},
 			zoom: 6
@@ -45,15 +56,26 @@ var SurvivalMap = React.createClass({
 		var userMarker = new google.maps.Marker({
     		position: userCoords,
    			map: this.mapImage,
-   			title: 'Your Location!'
+   			title: 'Your Location'
    		});
    		var infectionIcon = '/assets/images/infection-icon-30px.png'
    		var infectionMarker = new google.maps.Marker({
    			position: infectionCoords,
    			map: this.mapImage,
    			title: 'Infection Breakout Point',
-   			icon: infectionIcon
-   		})
+   			icon: infectionIcon,
+   			optimized: false,
+   		});
+   		var safeIcon = '/assets/images/assembly-point-icon-30px.png'
+   		for(var i = 0; i < safePlaceCoords.length; i++) {
+   			var safePlace = safePlaceCoords[i];
+   			var safeMarker = new google.maps.Marker({
+    			position: safePlace,
+   				map: this.mapImage,
+   				title: 'Safe Place',
+   				icon: safeIcon
+   			});
+   		};
 	},
 
 	render: function() {
